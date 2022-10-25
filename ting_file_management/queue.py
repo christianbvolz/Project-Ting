a94d933f33a1475848a4048659493dadd1a84567
Christian Volz <christianbvolz@gmail.com>
2022-10-25 15:49:29 -0300

    req 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,22 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.__len__() > 0:
+            return self._data.pop(0)
+        else:
+            return None
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index <= self.__len__():
+            return self._data[index]
+        raise IndexError
+        
+
