d899ca5f770cb6dd9fa772ec46a4ec8411902241
Christian Volz <christianbvolz@gmail.com>
2022-10-25 16:37:26 -0300

    req 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -19,7 +19,11 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        print("Não há elementos")
+    else:
+        file_deleted = instance.dequeue()['nome_do_arquivo']
+        print(f"Arquivo {file_deleted} removido com sucesso")
 
 
 def file_metadata(instance, position):
